d952520b3a5b44f712e690955c6da6788ab7feaa
sobhan shokri <xd3vman@gmail.com>
2021-11-16 06:49:22 +0330

    add text database for save hashlist

--- source.py
+++ source.py
@@ -2,12 +2,12 @@ import telebot, requests
 from bs4 import BeautifulSoup
 from time import sleep
 #enter apikey from https://etherscan.io
-apikey = "GWQDD5TJ2ISEQBYKPGJZGMHXD3X4TK4EKZ"
+apikey = ""
 # enter public ETH Address for scan internal transaction
-public_address = "0x943f0f2b754b74631637f9e52f12591adf03c8be"
+public_address = ""
 
-channel_id = "-1001325599117"
-tel_token = "2112896052:AAHL0HyMK-BwWvhgMM1_hEBF-i77VZfRYVI"
+channel_id = "" 
+tel_token = ""
 bot = telebot.TeleBot(tel_token, parse_mode=None)
 
 url = "https://api.etherscan.io/api?module=account&action=txlistinternal&address={0}&apikey={1}".format(public_address,apikey)
@@ -29,40 +29,51 @@ while (True):
     result = r['result']
     if r['status'] == '1':
         for i in range(0,len(result)):
+
            # print(result[i])
-           tx_hash = result[i]
-           tx_hash = tx_hash['hash']
-           check_hash = tx_hash in hash_list
-           if check_hash == True:
+            tx_hash = result[i]
+            tx_hash = tx_hash['hash']
+            dbs_file_read = open("dbs.txt", "r")
+            for i in dbs_file_read.read().split("\n"):
+
+                hash_list.append(i)
+
+            check_hash = tx_hash in hash_list
+            if check_hash == True:
                #print("check hash is : ", tx_hash)
                pass
-           else :
-               hash_list.append(tx_hash)
-               req = requests.get(check_hash_url+tx_hash,headers=headers)
+            else :
+                dbs_file = open("dbs.txt","a")
+                dbs_file.write(tx_hash+'\n')
+                dbs_file.close()
+            #    hash_list.append(tx_hash)
+                req = requests.get(check_hash_url+tx_hash,headers=headers)
                
-               if (req.status_code !=200) :
+                if (req.status_code !=200) :
+
                    
                    
                    #print("req error ")
-                   pass
-               else:
+                    pass
+                else:
+                    
                    
                 
-                   soup = BeautifulSoup(req.text, 'html.parser')
-                   token_id = soup.find_all('span',{"class":"hash-tag text-truncate"})
-                   token_id = token_id[0]
-                   token_id = token_id['title']
-                   #print(token_id)
-                   price_value = soup.find("span",{"id":"ContentPlaceHolder1_spanValue"})
-                   price_value = price_value.text
-                   price_value = price_value.strip()
-                   price_value = price_value.split("  ")
-                   eth_value = price_value[0]
-                   dollar_value = price_value[1]
-                   hyper_link = "<a href='{}'>Check TxId</a>".format(check_hash_url+tx_hash)
-                   p = '''💰 New order 💰 💵\nPrice = {0} | {1} 💵\n⛓ {2}⛓\n💎Number ofe of  sold : {3} 💎'''.format(eth_value,dollar_value,hyper_link,token_id)
-                   #print(p)
-                   bot.send_message(channel_id,text=p,parse_mode='html')
+                    soup = BeautifulSoup(req.text, 'html.parser')
+                    token_id = soup.find_all('span',{"class":"hash-tag text-truncate"})
+                    token_id = token_id[0]
+                    token_id = token_id['title']
+                    #print(token_id)
+                    price_value = soup.find("span",{"id":"ContentPlaceHolder1_spanValue"})
+                    price_value = price_value.text
+                    price_value = price_value.strip()
+                    price_value = price_value.split("  ")
+                    eth_value = price_value[0]
+                    dollar_value = price_value[1]
+                    hyper_link = "<a href='{}'>Check TxId</a>".format(check_hash_url+tx_hash)
+                    p = '''💰 New order 💰\n 💵 Price = {0} | {1} 💵\n⛓ {2}⛓\n💎 sold : {3} 💎'''.format(eth_value,dollar_value,hyper_link,token_id)
+                    #print(p)
+                    bot.send_message(channel_id,text=p,parse_mode='html')
 
     else : 
         r = requests.get(url)
